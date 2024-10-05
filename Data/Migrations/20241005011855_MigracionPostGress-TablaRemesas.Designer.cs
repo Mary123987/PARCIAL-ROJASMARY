fde08aaf0d53d6e1224aeaeac18fbbd7fe051b23
Mary Rojas <maryrojascordova08@gmail.com>
2024-10-04 20:32:59 -0500

    Migracion TablaRemesas

--- Data/Migrations/20241005011855_MigracionPostGress-TablaRemesas.Designer.cs
+++ Data/Migrations/20241005011855_MigracionPostGress-TablaRemesas.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PARCIAL_ROJASMARY.Data;
@@ -11,9 +12,11 @@ using PARCIAL_ROJASMARY.Data;
 namespace PARCIAL_ROJASMARY.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241005011855_MigracionPostGress-TablaRemesas")]
+    partial class MigracionPostGressTablaRemesas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
